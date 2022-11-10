f5bff6e668e2008a5fe4ad342fe168c51d766907
NSJoker <laughofnsjoker@gmail.com>
2022-11-10 16:22:06 +0530

    Initial Commit

---
+++ CoffeeApp/CoffeeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeAppApp.swift
+//  CoffeeApp
+//
+//  Created by Chandrachudh K on 10/11/22.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
